d6e8938ca5e135267a8d4d9128b388cabbf88227
MOHANKUMAR T <mohan13081999@gmail.com>
2023-07-31 06:52:04 +0530

    GOK-134 | Add Avni concept domain to get info about concept

---
+++ avni/src/main/java/org/avni_integration_service/avni/domain/AvniConcept.java
@@ -0,0 +1,41 @@
+package org.avni_integration_service.avni.domain;
+
+public class AvniConcept {
+
+    private Integer id;
+    private String uuid;
+    private String name;
+    private String dataType;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDataType() {
+        return dataType;
+    }
+
+    public void setDataType(String dataType) {
+        this.dataType = dataType;
+    }
+}
